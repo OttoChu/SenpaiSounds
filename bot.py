77d795f57dd6a01cf697efd5141b9767c0ed740a
OttoChu <ottoccl2004@gmail.com>
2024-09-12 16:44:25 +0800

    Added notice for edited and deleted message

--- bot.py
+++ bot.py
@@ -9,14 +9,38 @@ load_dotenv()
 
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
+# Log when the bot is ready
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
     try: 
         synced = await bot.tree.sync()
-        print(f"Synced {len(synced)} commands")
+        print(f"Synced {len(synced)} slash commands")
     except Exception as e:
         print(f"Failed to sync commands: {e}")
+
+# Log when a message is deleted
+@bot.event
+async def on_message_edit(before, after):
+    if before.author.bot:
+        return
+    emb = discord.Embed(title=f"I saw what you did {before.author.name}!", color=0xFF0000)
+    emb.add_field(name="Before", value=before.content, inline=False)
+    emb.add_field(name="After", value=after.content, inline=False)
+    emb.add_field(name="** **", value=f"[Jump to message]({after.jump_url})", inline=False)
+    emb.set_footer(text=f"Edited in {before.channel.name} by {before.author.name}")
+    await before.channel.send(embed=emb)
+
+# Log when a message is deleted
+@bot.event
+async def on_message_delete(message):
+    if message.author.bot:
+        return
+    emb = discord.Embed(title=f"Someone is trying to be sneaky!", color=0xFF0000)
+    emb.add_field(name="The following message was deleted", value=message.content, inline=False)
+    emb.set_footer(text=f"Originally sent by {message.author.name} in {message.channel.name}")
+
+    await message.channel.send(embed=emb)
 
 @bot.tree.command(name='hello', description='Say hello to the bot')
 async def hello(interaction: discord.Interaction):
